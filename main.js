1e610c19fff1917d6688a6a5ef30c4278b335f1a
xJosee <xJosee@github.com>
2020-07-20 16:16:44 -0600

    refactor

--- main.js
+++
@@ -1,9 +0,0 @@
-// Creamos un array vacio
-var ElementosClick = new Array();
-// Capturamos el click y lo pasamos a una funcion
-document.onclick = captura_click;
-	
-function captura_click(e) {
-    e.target.innerHTML = "X";
-	console.log(e.target);	
-}
